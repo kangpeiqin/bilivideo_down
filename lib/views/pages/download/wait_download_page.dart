f09fde43e48d0b963edf1a3471b4d30929b97175
stefan <1006181408@qq.com>
2024-08-13 00:17:46 +0800

    fix: macOS download path permission

--- lib/views/pages/download/wait_download_page.dart
+++ lib/views/pages/download/wait_download_page.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
+import 'package:bilivideo_down/constant/sp_key.dart';
 import 'package:bilivideo_down/provider/downloading_provider.dart';
 import 'package:bilivideo_down/provider/wait_download_provider.dart';
 import 'package:bilivideo_down/views/nav/download_nav.dart';
 import 'package:bilivideo_down/views/widget/video_card_widget.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sp_util/sp_util.dart';
 
 class WaitDownloadPage extends ConsumerStatefulWidget {
   const WaitDownloadPage({super.key});
@@ -39,7 +44,14 @@ class _WaitDownloadPageState extends ConsumerState<WaitDownloadPage> {
                     downloadService.removeAt(reversedIndex);
                     setState(() {});
                   },
-                  onDownload: () {
+                  onDownload: () async {
+                    if (Platform.isMacOS) {
+                      bool accessStatus = await handlePathSelection(context);
+                      if (accessStatus == false) {
+                        return;
+                      }
+                    }
+
                     downloadingService.addUnique(
                         waitDownloadState.videoInfoList[reversedIndex]);
                     final downloadNavigationState = context
@@ -55,4 +67,64 @@ class _WaitDownloadPageState extends ConsumerState<WaitDownloadPage> {
       ),
     );
   }
+}
+
+Future<bool> handlePathSelection(BuildContext context) async {
+  String? storagePath = SpUtil.getString(SpKey.storagePath);
+
+  if (storagePath == null ||
+      storagePath.isEmpty ||
+      !(await checkDirectoryAccess(storagePath))) {
+    final selectedPath = await FilePicker.platform.getDirectoryPath();
+
+    if (selectedPath != null) {
+      SpUtil.putString(SpKey.storagePath, selectedPath);
+      return true;
+    } else {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (context.mounted) {
+          showAccessDialog(context, '下载路径选择', '请选择下载路径');
+        }
+      });
+      return false;
+    }
+  }
+  return true;
+}
+
+Future<bool> checkDirectoryAccess(String path) async {
+  final directory = Directory(path);
+
+  if (!await directory.exists()) {
+    return false;
+  }
+
+  try {
+    final testFile = File('${directory.path}/.test_permission');
+    await testFile.writeAsString('test');
+    await testFile.delete();
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+void showAccessDialog(BuildContext context, String title, String content) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('确定'),
+          ),
+        ],
+      );
+    },
+  );
 }
