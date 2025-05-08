ac3b4c52b58d92147c33f3b1b3ecf4d38ab0f621
kangpeiqin <1060647301@qq.com>
2025-05-08 21:56:02 +0800

    refactor(scripts): 修改打包文件的保存路径

--- scripts/compile_windows_zip.ps1
+++ scripts/compile_windows_zip.ps1
@@ -1,4 +1,4 @@
-
+path: bilivideo_down.zip
 flutter clean
 flutter pub get
 flutter build windows
@@ -12,9 +12,9 @@ Copy-Item -Path "build\windows\x64\runner\Release\*" -Destination $tempDir -Recu
 Copy-Item -Path "sqlite3.dll" -Destination $tempDir -Force
 Write-Output 'Copied sqlite3.dll to package directory'
 
-# 直接将编译好的文件打包成 bilivideo_down.zip
-Compress-Archive -Path "$tempDir\*" -DestinationPath "bilivideo_down.zip" -Force
-Compress-Archive -Path "$tempDir\*" -DestinationPath "biliVidoDown-windows-x86-64.zip" -Force
+# 直接将编译好的文件打包成 bilivideo_down.zip，保存到 build/windows/ 目录
+Compress-Archive -Path "$tempDir\*" -DestinationPath "build\windows\bilivideo_down.zip" -Force
+Compress-Archive -Path "$tempDir\*" -DestinationPath "build\windows\biliVidoDown-windows-x86-64.zip" -Force
 
 # 清理临时目录
 Remove-Item -Path $tempDir -Recurse -Force
